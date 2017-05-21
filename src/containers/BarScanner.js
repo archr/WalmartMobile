b9a39907fc2128c0276f411a951e9705cf9e52ef
Antonio <berser.ar@gmail.com>
2017-05-20 19:50:30 -0500

    Added bar scanner

--- src/containers/BarScanner.js
+++ src/containers/BarScanner.js
@@ -1,12 +1,64 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar, TouchableWithoutFeedback, StyleSheet } from 'react-native'
+import Camera from 'react-native-camera'
 
 export default class BarScanner extends React.Component {
+  onPress = () => {
+    console.log('onPress')
+  }
+
   render () {
     return (
-      <View>
-        <Text>Bar</Text>
+      <View style={styles.container}>
+        <StatusBar backgroundColor='#2684e2' barStyle='light-content' />
+        <View style={styles.content}>
+          <Text style={styles.text}>Para escanear enfoca tu código de barras de tu ticket</Text>
+          <Camera
+            ref={cam => {
+              this.camera = cam
+            }}
+            style={styles.preview}
+            aspect={Camera.constants.Aspect.fill}
+            onBarCodeRead={() => console.log('onBarCodeRead')}
+          />
+
+          <TouchableWithoutFeedback onPress={this.onPress}>
+            <View style={styles.button} />
+          </TouchableWithoutFeedback>
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1B75CF'
+  },
+  content: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 30
+  },
+  text: {
+    color: 'white',
+    margin: 40,
+    marginTop: 80,
+    fontSize: 16,
+    textAlign: 'center'
+  },
+  preview: {
+    width: 320,
+    height: 160,
+    borderWidth: 2,
+    borderColor: '#FFF',
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  button: {
+    height: 160,
+    width: 320
+  }
+})
